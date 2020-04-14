e02c7d24a997cc9401f8dec88b84bf40533a8eb5
Eddie Kao <eddie@digik.com.tw>
2020-04-14 18:11:44 +0800

    update gem versions

--- tailwind.rb
+++ tailwind.rb
@@ -22,11 +22,11 @@ end
 set_source_path
 
 gem_group :development, :test do
-  gem 'foreman', '~> 0.86.0'
+  gem 'foreman', '~> 0.87.1'
   gem 'hirb-unicode', '~> 0.0.5'
-  gem 'rspec-rails', '~> 3.9'
+  gem 'rspec-rails', '~> 4.0'
   gem 'factory_bot_rails', '~> 5.1', '>= 5.1.1'
-  gem 'faker', '~> 2.10'
+  gem 'faker', '~> 2.11'
   gem 'pry-rails', '~> 0.3.9'
 end
 
